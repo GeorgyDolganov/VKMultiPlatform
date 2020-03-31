0c5eb68371d407ca707b592f499b408a67de4506
Georgy Dolganov <mr.craaazy@gmail.com>
2020-03-31 10:59:52 +0500

    Testing VkBridge functionality

--- VKMultiPlatform/index.js
+++ VKMultiPlatform/index.js
@@ -5,16 +5,10 @@ class VKMP {
     }
 
     Start() {
-        VK.init(function() {
-            console.log('Все работает. ' + 'Локацния: ' + window.location.hostname);
-         }, function() {
-            console.error(error);
-       }, '5.103');
-       document.getElementById("hostname").innerHTML = 
-        "Это принадлежит домену: " + window.location.hostname;
+        vkBridge.send('VKWebAppInit');
     }
     
     callMethod(){
-        VK.callMethod("showSettingsBox", 8214);
+        vkBridge.send("VKWebAppAllowNotifications", {});
     }
 };
